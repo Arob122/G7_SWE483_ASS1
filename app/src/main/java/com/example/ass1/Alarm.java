f1cb2d535f5c9a7854ecc4cb07996023ddcf861b
a-alsultan <arob2604@gmail.com>
2021-03-24 17:56:14 +0300

    الحمدلله خلصنا باقي فقط ننظف الكود الآن أنا وديم في بث رجعنا من اختبار السلم ثم بدينا وهاقد خرجت النتائج ولله الحمد والم

--- app/src/main/java/com/example/ass1/Alarm.java
+++ app/src/main/java/com/example/ass1/Alarm.java
@@ -3,39 +3,108 @@ package com.example.ass1;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.util.Random;
+
 
 public class Alarm extends BroadcastReceiver {
 
     private NotificationManagerCompat notificationManger;
+    public int getRandom(){
+        Random random = new Random();
+        int m = random.nextInt(9999 - 1000) + 1000;
+        return m;
+    }
     @Override
-    public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context,"Wake up",Toast.LENGTH_LONG).show();
+    public void onReceive(Context context, Intent inten) {
+        String title = "", msg = "", imp = "";
+        int myYear=0, myMonth=0, myday=0, myHour=0, myMinute=0;
+        int rand = 0;
+
         Log.d("myTag", "This is my message");
 
+        int random=getRandom();
+
+
+        Bundle extras = inten.getExtras();
+        if (extras != null) {
+            Log.d("myTag", "in extras");
+            title = extras.getString("title");
+            Log.d("myTag", "in extras title "+title);
+            msg = extras.getString("msg");
+            Log.d("myTag", "in extras msg "+msg);
+            imp = extras.getString("importence");
+            Log.d("myTag", "in extras importence "+imp);
+            rand = extras.getInt("random");
+            Log.d("myTag", "in extras random "+rand);
+            myYear = extras.getInt("myYear");
+            Log.d("myTag", "in extras myYear "+myYear);
+            myMonth = extras.getInt("myMonth");
+            Log.d("myTag", "in extras myMonth "+myMonth);
+            myday = extras.getInt("myday");
+            Log.d("myTag", "in extras myday "+myday);
+            myHour = extras.getInt("myHour");
+            Log.d("myTag", "in extras myHour "+myHour);
+            myMinute = extras.getInt("myMinute");
+            Log.d("myTag", "in extras myMinute "+myMinute);
+            
+            //The key argument here must match that used in the other activity
+        }
+        
         notificationManger = NotificationManagerCompat.from(context);
-        String title = "Hi";
-        String msg = "Iam notification";
-        Intent i = new Intent(context,MainActivity.class);
-        Notification notification = new NotificationCompat.Builder(context, MainActivity.CHANNEL_1_ID)
-                .setSmallIcon(R.drawable.button)
-                .setContentTitle(title)
-                .setContentText(msg)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setCategory(NotificationCompat.CATEGORY_MESSAGE)
-                .build();
+        Intent intent=new Intent(context, MainActivity.class);
+        intent.putExtra("title", title);
+        intent.putExtra("msg", msg);
+        intent.putExtra("importence", imp);
+        intent.putExtra("myYear", myYear);// year
+        intent.putExtra("myMonth", myMonth);// month
+        intent.putExtra("myday", myday);// day
+        intent.putExtra("myHour", myHour);// houer
+        intent.putExtra("myMinute", myMinute);// mint
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, rand+1, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        
 
 
-        notificationManger.notify(1,notification );
 
+        if (imp.equals("high")) {
+            Log.d("myTag", "in class Alarm high ");
+            Notification notification = new NotificationCompat.Builder(context, MainActivity.CHANNEL_1_ID)
+                    .setSmallIcon(R.drawable.ic_high_alarm)
+                    .setContentTitle(title)
+                    .setContentText(msg)
+                    .setContentIntent(pendingIntent)
+                    .build();
+
+
+
+            notificationManger.notify(random,notification );
+
+
+        }else {
+            Log.d("myTag", "in class Alarm low ");
+            Notification notification = new NotificationCompat.Builder(context, MainActivity.CHANNEL_2_ID)
+                    .setSmallIcon(R.drawable.ic_low_alarm_24)
+                    .setContentTitle(title)
+                    .setContentText(msg)
+                    .setContentIntent(pendingIntent)
+                    .build();
+
+
+
+            notificationManger.notify(random,notification );
+
+        }
     }
 
 }
--- app/src/main/java/com/example/ass1/MainActivity.java
+++ app/src/main/java/com/example/ass1/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.format.DateFormat;
@@ -18,6 +19,7 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
@@ -26,17 +28,21 @@ import androidx.core.app.NotificationManagerCompat;
 import com.allyants.notifyme.NotifyMe;
 
 import java.util.Calendar;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
     TextView Title1,monnth1,day1,hour1,min1,yeear1;
-    EditText Title;
-    Button Pickbutton;
+    EditText Title,detais;
+    Button Pickbutton, saveBtn,lowImportance, highImportance;
     int day, month, year, hour, minute;
     int myday, myMonth, myYear, myHour, myMinute;
     String enteredTitle;
     DatePickerDialog datePickerDialog;
     TimePickerDialog timePickerDialog;
     public static final String CHANNEL_1_ID = "channel1";
+    public static final String CHANNEL_2_ID = "channel2";
+    NotificationManager manager;
+    static String importence;
 
 
     private Calendar calendar;//for notify
@@ -49,14 +55,20 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         setContentView(R.layout.activity_main);
         Title1 = findViewById(R.id.Title);
         Pickbutton = findViewById(R.id.btnPick);
+        saveBtn = findViewById(R.id.saveBtn);
+        lowImportance = findViewById(R.id.lowImportance);
+        highImportance = findViewById(R.id.highImportance);
         Title=findViewById(R.id.textE);
+        detais=findViewById(R.id.detail);
         monnth1=findViewById(R.id.monnth);
         day1=findViewById(R.id.day);
         hour1=findViewById(R.id.hour);
         min1=findViewById(R.id.min);
         yeear1=findViewById(R.id.yeear);
         enteredTitle = Title.getText().toString();
+        importence="";
 
+        setData();
         Pickbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -64,13 +76,86 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
                 year = calendar.get(Calendar.YEAR);
                 month = calendar.get(Calendar.MONTH);
                 day = calendar.get(Calendar.DAY_OF_MONTH);
-                 datePickerDialog = new DatePickerDialog(MainActivity.this,MainActivity.this,year, month,day);
+                datePickerDialog = new DatePickerDialog(MainActivity.this,MainActivity.this,year, month,day);
                 datePickerDialog.show();
             }
         });
 
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-   }
+                Log.d("myTag", "This is saveBtn importance " + importence);
+                if(Title.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "Empty Title, please fill title", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(importence.equals("")){
+                    Toast.makeText(getApplicationContext(), "choose importance", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(yeear1.getText().toString().equals("")||
+                        monnth1.getText().toString().equals("")||
+                        day1.getText().toString().equals("")||
+                        hour1.getText().toString().equals("")||
+                        min1.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(), "fill date & time", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                setAlarmNotify();
+                Toast.makeText(getApplicationContext(), "Notification saved", Toast.LENGTH_LONG).show();
+                yeear1.setText("");
+                monnth1.setText("");
+                day1.setText("");
+                hour1.setText("");
+                min1.setText("");
+                Title.setText("");
+                detais.setText("");
+
+
+                lowImportance.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+                highImportance.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+
+            }
+
+        });
+
+
+        highImportance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                importence= "high";
+                highImportance.setBackgroundColor(Color.parseColor("#FF9795"));
+                lowImportance.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+            }
+        });
+        lowImportance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                importence= "low";
+                lowImportance.setBackgroundColor(Color.parseColor("#FFE00D"));
+                highImportance.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        setData();
+        Log.d("myTag", "in onResume  ");
+    }
+
+
+    @Override
+    protected void onNewIntent(Intent intent)
+    {
+        super.onNewIntent(intent);
+        setIntent(intent);
+        Log.d("myTag", "in onNewIntent  ");
+    }
     @Override
     public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
         myYear = year;
@@ -87,8 +172,9 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         hour = c.get(Calendar.HOUR);
         minute = c.get(Calendar.MINUTE);
 
-         timePickerDialog = new TimePickerDialog(MainActivity.this, MainActivity.this, hour, minute, DateFormat.is24HourFormat(this));
+        timePickerDialog = new TimePickerDialog(MainActivity.this, MainActivity.this, hour, minute, DateFormat.is24HourFormat(this));
         timePickerDialog.show();
+        Log.d("myTag", "in onDateSet calendar.getTimeInMillis() "+calendar.getTime());
     }
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
@@ -103,24 +189,65 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
 
         //to use in notify
-        calendar.set(Calendar.HOUR, myHour);//HOUR_OF_DAY
-        calendar.set(Calendar.MINUTE, myMinute);
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);//HOUR_OF_DAY
+        calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, 0);
         //end
-        setAlarmNotify();
+
+        Log.d("myTag", "in onTimeSet calendar.getTimeInMillis() "+calendar.getTime());
+        Log.d("myTag", "in onTimeSet myday "+myday);
+        Log.d("myTag", "in onTimeSet myHour "+myHour);
+
+    }
+
+
+    public int getRandom(){
+        Random random = new Random();
+        int m = random.nextInt(9999 - 1000) + 1000;
+        return m;
     }
 
     public void setAlarmNotify(){
+
+        int random = getRandom();
+
         Intent intent=new Intent(MainActivity.this, Alarm.class);
-        PendingIntent p1=PendingIntent.getBroadcast(MainActivity.this,0, intent,0);
+        String title = Title.getText().toString();
+        String msg = detais.getText().toString();
+        intent.putExtra("title", title);
+        intent.putExtra("msg", msg);
+        intent.putExtra("importence", importence);
+        intent.putExtra("random", random);
+        intent.putExtra("myYear", myYear);// year
+        intent.putExtra("myMonth", myMonth);// month
+        intent.putExtra("myday", myday);// day
+        intent.putExtra("myHour", myHour);// houer
+        intent.putExtra("myMinute", myMinute);// mint
+        PendingIntent p1=PendingIntent.getBroadcast(MainActivity.this,random, intent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+
+
+
+        Log.d("myTag", "This is my message inside main "+calendar.getTimeInMillis());
+        AlarmManager a=(AlarmManager)getSystemService(ALARM_SERVICE);
+        a.set(AlarmManager.RTC,calendar.getTimeInMillis(),p1);
+
+        /* Log.d("myTag", "in setAlarmNotify first line");
+
+        Log.d("myTag", "in setAlarmNotify() importence "+importence);
+        //PendingIntent p1=PendingIntent.getBroadcast(MainActivity.this,0, intent,0);
+        PendingIntent p1=PendingIntent.getActivity(MainActivity.this,1, intent,PendingIntent.FLAG_UPDATE_CURRENT);
 
         //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_SINGLE_TOP);
         pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
         Log.d("myTag", "This is my message inside main");
         AlarmManager a=(AlarmManager)getSystemService(ALARM_SERVICE);
         a.set(AlarmManager.RTC,calendar.getTimeInMillis(),p1);
+        Log.d("myTag", "in setAlarmNotify calendar.getTimeInMillis() "+calendar.getTime());
+        //a.set(AlarmManager.RTC,1000,p1);*/
     }
-    private void createNotificationChannels() {
+    private void createNotificationChannels(){
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
             NotificationChannel channel1 = new NotificationChannel(
                     CHANNEL_1_ID,
@@ -129,8 +256,66 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
             );
             channel1.setDescription("This is Channel 1");
 
-            NotificationManager manager = getSystemService(NotificationManager.class);
+            NotificationChannel channel2 = new NotificationChannel(
+                    CHANNEL_2_ID,
+                    "My Channel 2",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel2.setDescription("This is Channel 2");
+
+            manager = getSystemService(NotificationManager.class);
             manager.createNotificationChannel(channel1);
+            manager.createNotificationChannel(channel2);
         }
     }
+
+    public void setData(){
+        String title = "", msg = "", imp = "";
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            Log.d("myTag", "in extras");
+            title = extras.getString("title");
+            Log.d("myTag", "in setData extras title "+title);
+            msg = extras.getString("msg");
+            Log.d("myTag", "in setData extras msg "+msg);
+            imp = extras.getString("importence");
+            Log.d("myTag", "in setData extras importence "+imp);
+            myYear = extras.getInt("myYear");
+            Log.d("myTag", "in setData extras myYear "+myYear);
+            myMonth = extras.getInt("myMonth");
+            Log.d("myTag", "in setData extras myMonth "+myMonth);
+            myday = extras.getInt("myday");
+            Log.d("myTag", "in setData extras myday "+myday);
+            myHour = extras.getInt("myHour");
+            Log.d("myTag", "in setData extras myHour "+myHour);
+            myMinute = extras.getInt("myMinute");
+            Log.d("myTag", "in setData extras myMinute "+myMinute);
+
+            //The key argument here must match that used in the other activity
+            yeear1.setText(myYear+ "\n" );
+            monnth1.setText( myMonth + "\n") ;
+            day1.setText( myday + "\n") ;
+            hour1.setText( myHour + "\n") ;
+            min1.setText( myMinute + "\n") ;
+            Title.setText(title);
+            detais.setText(msg);
+
+            if (imp.equals("high")) {
+                highImportance.setBackgroundColor(Color.parseColor("#FF9795"));
+                lowImportance.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+
+            }else{
+                lowImportance.setBackgroundColor(Color.parseColor("#FFE00D"));
+                highImportance.setBackgroundColor(Color.parseColor("#00FFFFFF"));
+            }
+
+        }
+
+
+
+    }
+
+
 }
